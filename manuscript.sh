9f1c79d05c18d5b1d855e509140da9f7983619ad
Alistair Miles <alimanfoo@googlemail.com>
2020-09-02 11:11:54 +0100

    wip gwss artwork

---
+++ manuscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# clean
+rm -v supplemental.aux
+rm -v supplemental.bbl
+rm -v supplemental.bcf
+rm -v supplemental.blg
+rm -v supplemental.log
+rm -v supplemental.out
+rm -v supplemental.pdf
+rm -v supplemental.run.xml
+rm -v supplemental.synctex.gz
+
+# ensure bail out on error
+set -eo pipefail
+
+# setup environment
+source binder/env.sh
+
+# run pdflatex + biber + pdflatex
+pdflatex -interaction=nonstopmode -halt-on-error supplemental.tex
+biber supplemental
+pdflatex -interaction=nonstopmode -halt-on-error supplemental.tex
