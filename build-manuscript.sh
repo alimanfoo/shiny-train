4133d0d17c8a405d109f1b82dbc12411fa8c848f
Alistair Miles <alimanfoo@googlemail.com>
2020-10-22 22:47:01 +0100

    wip main figs

--- build-manuscript.sh
+++ build-manuscript.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
 
-# clean
-rm -v supplemental.aux
-rm -v supplemental.bbl
-rm -v supplemental.bcf
-rm -v supplemental.blg
-rm -v supplemental.log
-rm -v supplemental.out
-rm -v supplemental.lof
-rm -v supplemental.lot
-rm -v supplemental.toc
-rm -v supplemental.pdf
-rm -v supplemental.run.xml
-rm -v supplemental.synctex.gz
-
 # ensure bail out on error
 set -eo pipefail
 
 # setup environment
 source binder/env.sh
 
+# clean
+rm -vf supplemental.aux
+rm -vf supplemental.bbl
+rm -vf supplemental.bcf
+rm -vf supplemental.blg
+rm -vf supplemental.log
+rm -vf supplemental.out
+rm -vf supplemental.lof
+rm -vf supplemental.lot
+rm -vf supplemental.toc
+rm -vf supplemental.pdf
+rm -vf supplemental.run.xml
+rm -vf supplemental.synctex.gz
+
 # run pdflatex + biber + pdflatex
 pdflatex -interaction=nonstopmode -halt-on-error supplemental.tex
 biber supplemental
 pdflatex -interaction=nonstopmode -halt-on-error supplemental.tex
+
+# clean
+rm -vf main.aux
+rm -vf main.bbl
+rm -vf main.bcf
+rm -vf main.blg
+rm -vf main.log
+rm -vf main.out
+rm -vf main.lof
+rm -vf main.lot
+rm -vf main.toc
+rm -vf main.pdf
+rm -vf main.run.xml
+rm -vf main.synctex.gz
+
+# run pdflatex + biber + pdflatex
+pdflatex -interaction=nonstopmode -halt-on-error main.tex
+biber main
+pdflatex -interaction=nonstopmode -halt-on-error main.tex
